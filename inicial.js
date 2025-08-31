faeb162eaf1fb51c90d81456fb0d0a62d84c1a11
Letícia Gabrielly da Silva <165218856+LetGabs@users.noreply.github.com>
2025-08-31 12:08:21 -0300

    galeria+hero+edicao

--- inicial.js
+++ inicial.js
@@ -35,3 +35,25 @@ form.addEventListener("submit", function(event) {
     form.submit();
 })
     
+const track = document.querySelector('.carousel-track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel-btn.next');
+const prevButton = document.querySelector('.carousel-btn.prev');
+let currentIndex = 0;
+
+function updateSlidePosition() {
+  const slideWidth = slides[0].getBoundingClientRect().width;
+  track.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+}
+
+nextButton.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % slides.length;
+  updateSlidePosition();
+});
+
+prevButton.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+  updateSlidePosition();
+});
+
+window.addEventListener('resize', updateSlidePosition);
